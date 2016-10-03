c6f07d283a96412e31132b1c52e381ccf41de3cc
Juani <nacho.mone@gmail.com>
2016-10-02 21:58:14 -0300

    Vista de clientes

--- tp/src/ar/edu/uade/tpapi/vista/CreaClientes.java
+++ tp/src/ar/edu/uade/tpapi/vista/CreaClientes.java
@@ -1,5 +1,7 @@
 package ar.edu.uade.tpapi.vista;
 
+import java.util.Vector;
+
 import ar.edu.uade.tpapi.controlador.Controlador;
 
 public class CreaClientes {
@@ -13,6 +15,14 @@ public class CreaClientes {
 			c.altaCliente(i,"Cliente"+Integer.toString(i),"DomCliente"+Integer.toString(i)
 					,Integer.toString(i),"cliente"+Integer.toString(i)+"@clientes.com");
 		}
-		c.listarClientes();
+		Vector<ClienteView> clientesViewTmp = c.listarClientes();
+		for(int i=0;i<clientesViewTmp.size();i++){
+			System.out.println(clientesViewTmp.get(i).getDniCliente());
+			System.out.println(clientesViewTmp.get(i).getNombre());
+			System.out.println(clientesViewTmp.get(i).getDomicilio());
+			System.out.println(clientesViewTmp.get(i).getTelefono());
+			System.out.println(clientesViewTmp.get(i).getMail());
+			System.out.println(clientesViewTmp.get(i).isActivo());
+		}
 	}
 }
